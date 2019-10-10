d4a5d7bd98149704a69b5cbcf51dfa68e48310f4
kounat <dchen2604@gmail.com>
2019-10-10 17:19:19 -0400

    Add endpoints and consistency fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
-const host = "0.0.0.0";
+const host = '0.0.0.0';
 const port = 80;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/adduser', (req, res) => {});
+
+app.post('/login', (req, res) => {});
+
+app.post('/logout', (req, res) => {});
+
+app.post('/verify', (req, res) => {});
+
+app.post('/additem', (req, res) => {});
+
+app.get('/item/:itemid', (req, res) => { res.send('Your item id is: ' + req.params.itemid); });
+
+app.post('/search', (req, res) => {});
 
 app.listen(port, host, () => console.log(`Listening on port ${port}!`));
