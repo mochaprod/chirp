a452be0eb1f40ae4acda7b42136ec73ab71ee54c
Ben Huang <ben.huang006@gmail.com>
2019-10-16 16:51:10 -0400

    Custom port

--- packages/chirp-api/src/app.js
+++ packages/chirp-api/src/app.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const host = '0.0.0.0';
-const port = 80;
+const host = "0.0.0.0";
+const port = 6969;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post('/adduser', (req, res) => {});
+app.post("/adduser", (req, res) => {});
 
-app.post('/login', (req, res) => {});
+app.post("/login", (req, res) => {});
 
-app.post('/logout', (req, res) => {});
+app.post("/logout", (req, res) => {});
 
-app.post('/verify', (req, res) => {});
+app.post("/verify", (req, res) => {});
 
-app.post('/additem', (req, res) => {});
+app.post("/additem", (req, res) => {});
 
-app.get('/item/:itemid', (req, res) => { res.send('Your item id is: ' + req.params.itemid); });
+app.get("/item/:itemid", (req, res) => {
+    res.send("Your item id is: " + req.params.itemid);
+});
 
-app.post('/search', (req, res) => {});
+app.post("/search", (req, res) => {});
 
 app.listen(port, host, () => console.log(`Listening on port ${port}!`));
