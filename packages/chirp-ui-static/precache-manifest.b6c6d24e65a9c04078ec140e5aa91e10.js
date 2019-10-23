0aaf5ac6ee68917bfc6ab42f2275ecfdb19e504e
Ben Huang <ben.huang006@gmail.com>
2019-10-23 18:20:02 -0400

    Static UI

---
+++ packages/chirp-ui-static/precache-manifest.b6c6d24e65a9c04078ec140e5aa91e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f5133afb777936a6a163626b20b229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd147688a71d410a483",
+    "url": "/static/css/main.9024a637.chunk.css"
+  },
+  {
+    "revision": "9241f8a4622e65ed943c",
+    "url": "/static/js/2.408e6150.chunk.js"
+  },
+  {
+    "revision": "50ca9a36c99fbf1a7418",
+    "url": "/static/js/3.5f7710a7.chunk.js"
+  },
+  {
+    "revision": "8362d894df52aa0fd9b9",
+    "url": "/static/js/4.4a183dcd.chunk.js"
+  },
+  {
+    "revision": "1dd147688a71d410a483",
+    "url": "/static/js/main.fa961f9e.chunk.js"
+  },
+  {
+    "revision": "3812079314018d2c9455",
+    "url": "/static/js/runtime-main.b81a6758.js"
+  }
+]);
