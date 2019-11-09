ae0708d727916dcbc48b6cbd3956811ecbb3812d
kounat <dchen2604@gmail.com>
2019-11-09 10:48:32 -0500

    Set up Mongo config servers

---
+++ packages/chirp-mongodb/config/primary.js
@@ -0,0 +1,14 @@
+rs.initiate({
+  _id: "configReplSet",
+  configsvr: true,
+  members: [
+    {
+      _id: 0,
+      host: "config1:27019"
+    },
+    {
+      _id: 1,
+      host: "config2:27019"
+    }
+  ]
+});
