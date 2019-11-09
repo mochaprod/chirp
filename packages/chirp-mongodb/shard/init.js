3cbcf57354ca4ab75b7c409dc70945c13fcc724a
kounat <dchen2604@gmail.com>
2019-11-09 14:16:04 -0500

    Mongo sharding

---
+++ packages/chirp-mongodb/shard/init.js
@@ -0,0 +1,7 @@
+sh.addShard("shard1:27018")
+sh.addShard("shard2:27018")
+
+sh.enableSharding("chirp")
+sh.shardCollection("chirp.users", { "_id": "hashed" } )
+sh.shardCollection("chirp.items", { "_id": "hashed" } )
+sh.shardCollection("chirp.follows", { "_id": "hashed" } )
