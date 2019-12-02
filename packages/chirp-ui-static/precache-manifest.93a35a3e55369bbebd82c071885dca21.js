c94f48f1f2a6f74a53c1690f424c5884f7dca221
Ben Huang <ben.huang006@gmail.com>
2019-12-02 16:35:30 -0500

    Rebuilt UI

---
+++ packages/chirp-ui-static/precache-manifest.93a35a3e55369bbebd82c071885dca21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5170adf0be2c15dd29cf9c36f0d6af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebecada96023ab6c73b5",
+    "url": "/static/css/3.e428c6c6.chunk.css"
+  },
+  {
+    "revision": "b5293c9af6fa01e0d529",
+    "url": "/static/css/4.4ae1ce98.chunk.css"
+  },
+  {
+    "revision": "f5fe69b57eb49874db10",
+    "url": "/static/css/main.8a538530.chunk.css"
+  },
+  {
+    "revision": "bf05ff1eee2a1f220586",
+    "url": "/static/js/2.075a2b2b.chunk.js"
+  },
+  {
+    "revision": "ebecada96023ab6c73b5",
+    "url": "/static/js/3.8de855a6.chunk.js"
+  },
+  {
+    "revision": "b5293c9af6fa01e0d529",
+    "url": "/static/js/4.adab5355.chunk.js"
+  },
+  {
+    "revision": "f5fe69b57eb49874db10",
+    "url": "/static/js/main.f447e6e4.chunk.js"
+  },
+  {
+    "revision": "e3cfd91f473827b40714",
+    "url": "/static/js/runtime-main.fa3ed7fc.js"
+  }
+]);
